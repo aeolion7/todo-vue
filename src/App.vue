5c906a611565c92554dc4bf08098347a7db6fdee
Joseph Lionarons <lionsamurai@gmail.com>
2021-01-20 15:27:34 -0500

    render list of ToDoItems

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :id="item.id"
+          :label="item.label"
+          :done="item.done"
+        ></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import uniqueId from 'lodash.uniqueid';
+import ToDoItem from './components/ToDoItem';
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        {
+          id: uniqueId('todo-'),
+          label: 'Create a Vue project with the CLI',
+          done: true,
+        },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -18,5 +46,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+li {
+  list-style: none;
 }
 </style>
